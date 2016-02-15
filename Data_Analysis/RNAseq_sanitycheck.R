20ed1b355afc6a6dfabaa8547f998133459366d1
David-Rattray <dave.g.rattray@gmail.com>
2016-02-15 15:23:23 -0800

    Setup expressed data set so col 1 has all the genes and col 2-5 has the mean rpkm_value for each of the 4 treatment conditions

--- Data_Analysis/RNAseq_sanitycheck.R
+++ Data_Analysis/RNAseq_sanitycheck.R
@@ -27,8 +27,18 @@ rpkm.null  <- rpkm.null[ order(rpkm.null[,1], rpkm.null[,5]),]
 rpkm.summary <- table(rpkm.null[, c('genes', 'group.fct')], useNA='ifany')
 rpkm.freq <- as.data.frame(rpkm.summary)
 rpkm.freq <- subset(rpkm.freq, Freq==3)
-
+rpkm.freq <- dcast(rpkm.freq, genes~group.fct)
+  
 ## perhaps before here, need to setup so that the gene is followed by columns of its freq
 ## for each group condition for easier comparison
 rpkm.comp <- dcast(rpkm.null, genes~group.fct)
 
+## Analyzing expressed data subset
+expressed <- subset(row.merged, rpkm_value > 0.00001)
+summary(expressed)
+
+## Taking the mean of the replicates and grouping data by gene+group
+expressed.mean <- expressed %>% group_by(genes,group.fct) %>% summarize(rpkm.mean = mean(rpkm_value))
+
+## Transposing data so columns are the group.fct's
+expressed.mean <- dcast(expressed.mean, genes~group.fct)
